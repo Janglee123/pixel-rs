e575983d2ecd4a29538c72aeb728dbe3b80d27ec
Jang <jang@local.com>
2024-01-09 06:09:32 +0530

    Made Mesh Asset

--- src/plugins/renderer_plugins/mesh.rs
+++ src/plugins/renderer_plugins/mesh.rs
@@ -1,20 +1,20 @@
 use wgpu::Buffer;
 
-use crate::plugins::core::render_plugin::Gpu;
+use crate::plugins::core::{asset_storage::Asset, render_plugin::Gpu};
 
 use super::vertex::Vertex;
 use std::sync::Arc;
 
 #[derive(Debug)]
 pub struct Mesh {
-    pub vertices: Arc<[Vertex]>,
-    pub indices: Arc<[u16]>,
+    pub vertices: Box<[Vertex]>,
+    pub indices: Box<[u16]>,
 }
 
 impl Mesh {
     pub fn get_quad_mesh() -> Self {
         Self {
-            vertices: Arc::new([
+            vertices: Box::new([
                 Vertex {
                     position: [0.5, 0.5, 1.0],
                     color: [1.0, 1.0, 1.0],
@@ -32,13 +32,13 @@ impl Mesh {
                     color: [1.0, 1.0, 1.0],
                 },
             ]),
-            indices: Arc::new([0, 1, 2, 0, 2, 3]),
+            indices: Box::new([0, 1, 2, 0, 2, 3]),
         }
     }
 
     pub fn get_hex_mesh() -> Self {
         Self {
-            vertices: Arc::new([
+            vertices: Box::new([
                 Vertex {
                     position: [0.0, 0.5, 1.0],
                     color: [1.0; 3],
@@ -64,7 +64,16 @@ impl Mesh {
                     color: [1.0; 3],
                 },
             ]),
-            indices: Arc::new([0, 2, 1, 2, 4, 3, 0, 5, 4, 0, 4, 2]),
+            indices: Box::new([0, 2, 1, 2, 4, 3, 0, 5, 4, 0, 4, 2]),
+        }
+    }
+}
+
+impl Asset for Mesh {
+    fn from_binary(binary: Vec<u8>) -> Self {
+        Mesh {
+            vertices: Box::new([]),
+            indices: Box::new([]),
         }
     }
 }
